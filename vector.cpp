7b2b5866d01f29ad4403666bf9a85f5b31b82a12
sr5392 <61883265+sr5392@users.noreply.github.com>
2025-08-03 21:43:10 +0200

    Reformat source code

--- vector.cpp
+++ vector.cpp
@@ -2,30 +2,28 @@
 #include <utility>
 #include <algorithm>
 
+Vector::Vector() : m_size{0}, m_capacity{m_size}, m_data{nullptr} {
+}
 
-Vector::Vector() : m_size{0}, m_capacity{m_size}, m_data{nullptr} {}
-
-Vector::Vector(const std::size_t size) : m_size {size}, m_capacity{m_size}, m_data{new int[m_capacity]} {
+Vector::Vector(const std::size_t size) : m_size{size}, m_capacity{m_size}, m_data{new int[m_capacity]} {
     for (std::size_t i = 0; i < m_size; ++i) {
         m_data[i] = 0;
     }
 }
 
-Vector::Vector(const std::initializer_list<int> init) :
-m_size{init.size()},
-m_capacity{m_size},
-m_data{new int[m_capacity]} {
+Vector::Vector(const std::initializer_list<int> init) : m_size{init.size()},
+                                                        m_capacity{m_size},
+                                                        m_data{new int[m_capacity]} {
     std::copy(init.begin(), init.end(), m_data);
 }
 
-Vector::Vector(const Vector& other) :
-m_size{other.m_size},
-m_capacity{other.m_capacity},
-m_data{new int[m_size]} {
+Vector::Vector(const Vector &other) : m_size{other.m_size},
+                                      m_capacity{other.m_capacity},
+                                      m_data{new int[m_size]} {
     std::copy(other.m_data, other.m_data + m_size, m_data);
 }
 
-Vector& Vector::operator=(const Vector &other) {
+Vector &Vector::operator=(const Vector &other) {
     if (this == &other) return *this;
 
     if (m_capacity < other.m_capacity) {
@@ -39,15 +37,13 @@ Vector& Vector::operator=(const Vector &other) {
     return *this;
 }
 
-Vector::Vector(Vector&& other) noexcept :
-m_size{0},
-m_capacity{0},
-m_data{nullptr} {
+Vector::Vector(Vector &&other) noexcept : m_size{0},
+                                          m_capacity{0},
+                                          m_data{nullptr} {
     swap(other);
 }
 
-
-Vector& Vector::operator=(Vector&& other) noexcept {
+Vector &Vector::operator=(Vector &&other) noexcept {
     if (this == &other) return *this;
     swap(other);
 
@@ -61,7 +57,7 @@ Vector::~Vector() {
 void Vector::reserve(const std::size_t capacity) {
     if (capacity <= m_capacity) return;
 
-    int* data = new int[capacity];
+    int *data = new int[capacity];
     std::copy(m_data, m_data + m_size, data);
     delete[] m_data;
     m_data = data;
@@ -84,11 +80,11 @@ void Vector::push_back(const int i) {
     ++m_size;
 }
 
-int& Vector::operator[](const std::size_t i) {
+int &Vector::operator[](const std::size_t i) {
     return m_data[i];
 }
 
-const int& Vector::operator[](const std::size_t i) const {
+const int &Vector::operator[](const std::size_t i) const {
     return m_data[i];
 }
 
@@ -106,6 +102,6 @@ void Vector::swap(Vector &other) noexcept {
     std::swap(m_data, other.m_data);
 }
 
-void swap(Vector& a, Vector& b) noexcept {
+void swap(Vector &a, Vector &b) noexcept {
     a.swap(b);
 }
